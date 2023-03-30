266e0ad85e2612eacdee04bf86c36c87a6fc36a1
Francesco PACI <francesco.paci@greenwaves-technologies.com>
2023-03-30 17:42:16 +0200

    Added image stream for SPI Slave

--- gap_spi_slave/multi_spi.c
+++ gap_spi_slave/multi_spi.c
@@ -39,28 +39,14 @@
 #define SPI_SLAVE_PAD_FUNC              PI_PAD_FUNC2
 
 #define SPI_NO_OPTION                   0 // No option applied for SPI transfer
-// Data size to send
-#define CHUNK_SIZE  ( 320 )
-#define CHUNKS      ( 240 )
-#define CHUNK_SIZE_BITS (CHUNK_SIZE << 3) // Get buffer size expressed in bits
-#define BUFFER_SIZE ( CHUNK_SIZE * CHUNKS )
 
 static uint8_t debug = 0;
-static uint8_t *spi_master_tx_buf;
-static uint8_t *spi_master_rx_buf;
-static uint8_t *spi_slave_rx_buf;
+static uint8_t *tx_buffer;
 
-// GPIO Initialization ****************************************************************************
-
-static void spi_master_pad_init()
-{
-    pi_pad_function_set(SPI_MASTER_PAD_SCK, SPI_MASTER_PAD_FUNC);
-    pi_pad_function_set(SPI_MASTER_PAD_SDO, SPI_MASTER_PAD_FUNC);
-    pi_pad_function_set(SPI_MASTER_PAD_SDI, SPI_MASTER_PAD_FUNC);
-    pi_pad_mux_group_set(SPI_MASTER_PAD_CS1, PI_PAD_MUX_GROUP_SPI1_CS1);
-    pi_pad_function_set(SPI_MASTER_PAD_CS1, SPI_MASTER_PAD_FUNC);
-    pi_pad_function_set(SPI_MASTER_PAD_CS0, SPI_MASTER_PAD_FUNC);
-}
+#define IMG_W  ( 320 )
+#define IMG_H  ( 240 )
+#define BUFFER_SIZE ( IMG_W * IMG_H )
+#define CHUNK_SIZE_BITS (IMG_W << 3) // Get buffer size expressed in bits
 
 static void spi_slave_pad_init()
 {
@@ -69,30 +55,6 @@ static void spi_slave_pad_init()
     pi_pad_function_set(SPI_SLAVE_PAD_SDO, SPI_SLAVE_PAD_FUNC);
     pi_pad_function_set(SPI_SLAVE_PAD_SDI, SPI_SLAVE_PAD_FUNC);
 }
-
-// SPI Initialization *****************************************************************************
-
-// static void spi_master_init(pi_device_t* spi_master, struct pi_spi_conf* spi_master_conf)
-// {
-//     pi_assert(spi_master);
-//     pi_assert(spi_master_conf);
-
-//     pi_spi_conf_init(spi_master_conf);
-//     spi_master_conf->wordsize = SPI_MASTER_WORDSIZE;
-//     spi_master_conf->big_endian = SPI_MASTER_ENDIANESS;
-//     spi_master_conf->max_baudrate = SPI_MASTER_BAUDRATE;
-//     spi_master_conf->polarity = SPI_MASTER_POLARITY;
-//     spi_master_conf->phase = SPI_MASTER_PHASE;
-//     spi_master_conf->itf = SPI_MASTER_ITF;
-//     spi_master_conf->cs = SPI_MASTER_CS;
-//     spi_master_conf->dummy_clk_cycle = SPI_MASTER_DUMMY_CYCLE;
-//     spi_master_conf->dummy_clk_cycle_mode = SPI_MASTER_DUMMY_CYCLE_MODE;
-//     spi_master_conf->is_slave = SPI_MASTER_IS_SLAVE;
-//     pi_open_from_conf(spi_master, spi_master_conf);
-
-//     printf("INFO: Dummy clock cycle number : %d\n", spi_master_conf->dummy_clk_cycle);
-//     printf("INFO: Dummy clock cycle mode   : %d\n", spi_master_conf->dummy_clk_cycle_mode);
-// }
 
 static void spi_slave_init(pi_device_t* spi_slave, struct pi_spi_conf* spi_slave_conf)
 {
@@ -124,8 +86,7 @@ static void spi_slave_init(pi_device_t* spi_slave, struct pi_spi_conf* spi_slave
 static void receive_callback(void* context)
 {
 
-    //if(debug) 
-    printf("INFO: Slave SPI receive is finished\n");
+    if(debug) printf("INFO: Slave SPI receive is finished\n");
     pi_evt_push((pi_evt_t*)context);
 }
 
@@ -157,92 +118,28 @@ int main(void)
     if (pi_spi_open(&spi_slave))
     {
         printf("ERROR: Failed to open SPI peripheral\n");
-        retval = -1;
+        return -1;
     }
-    else
+
+    tx_buffer  = pi_l2_malloc(BUFFER_SIZE);
+
+    int iter=0;
+    while (1)
     {
-        //spi_master_tx_buf = pi_l2_malloc(BUFFER_SIZE);
-        //spi_master_rx_buf = pi_l2_malloc(BUFFER_SIZE);
-        spi_slave_rx_buf  = pi_l2_malloc(BUFFER_SIZE);
 
-        // Prepare data buffer to send
-        for (int i = 0; i < BUFFER_SIZE; i++)
-        {
-            //spi_master_tx_buf[i] = BUFFER_SIZE - i;
-            //spi_master_rx_buf[i] = 0;
-            spi_slave_rx_buf[i] = i%320;
-        }
+        //Create a syntethic image and change it every frame to evaluate framerate
+        for(int i=0;i<IMG_H;i++)
+            for(int j=0;j<IMG_W;j++)
+                tx_buffer[+i*IMG_W+j]=(iter%2)?(i):(255-i);
+        iter++;
 
-        // for (int i = 0; i < CHUNKS; i++)
-        // {
-        //     // Initialize a notification event to block this
-        //     // process until spi master send and spi slave receive
-        //     // operation are performed.
-        //     pi_evt_sig_init(&end_task);
-
-        //     // Initiate SPI slave receive
-        //     pi_spi_receive_async(&spi_slave, (spi_slave_rx_buf + (i*CHUNK_SIZE)), CHUNK_SIZE_BITS, SPI_NO_OPTION, pi_evt_callback_irq_init(&receive_task, &receive_callback, (void*)&end_task));
-
-        //     // Initiate SPI master send
-        //     //pi_spi_send_async(&spi_master, (spi_master_tx_buf + (i*CHUNK_SIZE)), CHUNK_SIZE_BITS, SPI_NO_OPTION, pi_evt_callback_irq_init(&send_task, &send_callback, NULL));
-
-        //     // Blocking the execution until send and receive steps are done.
-        //     pi_evt_wait(&end_task);
-
-        //     printf("Received somenthing:\n");
-        //     for(int a=0;a<CHUNK_SIZE;a+=4){
-        //         printf("0x%x 0x%x 0x%x 0x%x\n",spi_slave_rx_buf[i*CHUNK_SIZE+a+0],spi_slave_rx_buf[i*CHUNK_SIZE+a+1],spi_slave_rx_buf[i*CHUNK_SIZE+a+2],spi_slave_rx_buf[i*CHUNK_SIZE+a+3]);
-        //     }
-
-            
-        // }
-
-        printf("master -> slave test done\n");
-
-        for (int i = 0; i < CHUNKS; i++)
-        {
-            // Initialize a notification event to block this
-            // process until spi master receive and spi slave sent
-            // operation are performed.
+        for(int i=0;i<IMG_H;i++){
             pi_evt_sig_init(&end_task);
-
-            // Initiate SPI slave send
-            pi_spi_send_async(&spi_slave, spi_slave_rx_buf, CHUNK_SIZE_BITS, SPI_NO_OPTION, pi_evt_callback_irq_init(&send_task, &send_callback, &end_task));
-
-            // Initiate SPI master receive
-            //pi_spi_receive_async(&spi_master, (spi_master_rx_buf + (i*CHUNK_SIZE)), CHUNK_SIZE_BITS, SPI_NO_OPTION, pi_evt_callback_irq_init(&receive_task, &receive_callback, (void*)&end_task));
-
-            // Blocking the execution until send and receive steps are done.
+            pi_spi_send_async(&spi_slave, tx_buffer+(i*IMG_W), CHUNK_SIZE_BITS, SPI_NO_OPTION, pi_evt_callback_irq_init(&send_task, &send_callback, &end_task));
             pi_evt_wait(&end_task);
         }
-        printf("slave -> master test done\n");
-
-        // // Verify received data
-        // for(int i = 0; i < BUFFER_SIZE; i++)
-        // {
-        //     if(spi_master_tx_buf[i] != spi_master_rx_buf[i])
-        //     {
-        //         printf("ERROR: index (%ld), expected (0x%lx), got M:(0x%lx), S:(0x%lx)\n",
-        //             i, spi_master_tx_buf[i], spi_master_rx_buf[i], spi_slave_rx_buf[i]);
-        //         retval = -1;
-        //         break;
-        //     }
-        // }
-
-        // // Testing data shift
-        // if(retval)
-        // {
-        //     if(spi_master_tx_buf[1] == spi_slave_rx_buf[0])
-        //     {
-        //         printf("ERROR: First byte not read. All data are shifted by 1 byte\n");
-        //         printf("INFO: Wrong dummy clock cycle configuration\n");
-        //     }
-        // }
-        // else
-        // {
-        //     printf("INFO: Data received successfully\n");
-        // }
     }
+
     printf("\n\t *** End of test ***\n\n");
     return retval;
 }
